64e2e354b756c92a3a335a5d62e30dd21634c1fe
martinovici@rsm.nl <martinovici@rsm.nl>
2021-03-10 09:50:08 +0100

    tested (it worked) connection to the new Twitter API (recent search endpoint)

---
+++ Twitter_API/API2_recentsearch.R
@@ -0,0 +1,27 @@
+# make sure to install this package
+library(httr)
+
+# Replace the bearer token below with the bearer_token for your own app
+bearer_token = "thisisnotarealtoken"
+
+headers = c(
+    `Authorization` = sprintf('Bearer %s', bearer_token)
+)
+
+params = list(
+    `query` = 'from:twitterdev lang:en',
+    `max_results` = '10',
+    `tweet.fields` = 'created_at,lang,context_annotations'
+)
+
+response <- httr::GET(url = 'https://api.twitter.com/2/tweets/search/recent', httr::add_headers(.headers=headers), query = params)
+
+fas_body <-
+    content(
+        response,
+        as = 'parsed',
+        type = 'application/json',
+        simplifyDataFrame = TRUE
+    )
+
+View(fas_body$data)
